4af7c88ce7477d66cb3cde7f929ccac796e93366
Batuhannsari <batuhan.sari@aselsis.com.tr>
2024-02-17 19:53:27 +0300

    pages added and redirected path to "/products"  from "/"

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/products', // "/" adresi /products sayfasına yönlendirilecek
+        permanent: true, // Yönlendirme kalıcı olacak
+      },
+    ];
+  },
 };
 
 export default nextConfig;
