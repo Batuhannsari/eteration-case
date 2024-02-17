a02f04885afb582cee780ae33f50f9f5c2d63386
Batuhannsari <batuhan.sari@aselsis.com.tr>
2024-02-17 20:25:22 +0300

    types added

---
+++ src/types/ProductType.ts
@@ -0,0 +1,21 @@
+export type ProductType = {
+    createdAt: string
+    name: string
+    image: string
+    price: string
+    description: string
+    model: string
+    brand: string
+    id: string
+}
+
+export const ProductDefaultValue = {
+    createdAt: "",
+    name: "",
+    image: "",
+    price: "",
+    description: "",
+    model: "",
+    brand: "",
+    id: "",
+}
