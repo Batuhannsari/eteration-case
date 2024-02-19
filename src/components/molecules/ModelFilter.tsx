4f96e03269d5185723a77e9055e2bc06a3441825
Batuhannsari <batuhan.sari@aselsis.com.tr>
2024-02-19 03:29:57 +0300

    modelfilter update

--- src/components/molecules/ModelFilter.tsx
+++ src/components/molecules/ModelFilter.tsx
@@ -11,6 +11,27 @@ const ModelFilter: React.FC<IProps> = (props) => {
     const [uniqueModels, setUniqueModels] = useState<string[]>([]);
     const [filteredOptions, setFilteredOptions] = useState<string[]>([]);
 
+    useEffect(() => {
+        // Brandler seçili değilse tüm modelleri göster
+
+        if (filterStates.brand.length === 1) {
+            const allModels = products.map(product => product.model);
+            const uniqueModelsSet = new Set<string>(allModels);
+            const uniqueModelsArray = Array.from(uniqueModelsSet);
+            setUniqueModels(uniqueModelsArray);
+            setFilteredOptions(uniqueModelsArray);
+        } else {
+
+            // Brandler seçili ise, seçili brandlerin modellerini göster
+            const filteredProducts = products.filter(product => filterStates.brand.includes(product.brand));
+            const models = filteredProducts.map(product => product.model);
+            const uniqueModelsSet = new Set<string>(models);
+            const uniqueModelsArray = Array.from(uniqueModelsSet);
+            setUniqueModels(uniqueModelsArray);
+            setFilteredOptions(uniqueModelsArray);
+        }
+    }, [products, filterStates.brand]);
+
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         const filtered = uniqueModels.filter((option: string) =>
@@ -30,34 +51,13 @@ const ModelFilter: React.FC<IProps> = (props) => {
         }));
     };
 
-    useEffect(() => {
-        if (filterStates.brand.length === 1) {
-            // Eğer brandlerden hiçbir şey seçili değilse, tüm modelleri al
-            const allModels = products.map(product => product.model);
-            const uniqueModelsSet = new Set<string>(allModels);
-            const uniqueModelsArray = Array.from(uniqueModelsSet);
-            setUniqueModels(uniqueModelsArray);
-            setFilteredOptions(uniqueModelsArray);
-        } else {
-            // Brandlerle filtrelenmiş ürünlerin listesi
-            const filteredProducts = products.filter(product => filterStates.brand.includes(product.brand));
-            // Brandlerle filtrelenmiş ürünlerin modellerinin listesi
-            const models = filteredProducts.map(product => product.model);
-            // Tekrar eden modelleri kaldırarak benzersiz modellerin listesini oluştur
-            const uniqueModelsSet = new Set<string>(models);
-            const uniqueModelsArray = Array.from(uniqueModelsSet);
-            setUniqueModels(uniqueModelsArray);
-            setFilteredOptions(uniqueModelsArray);
-        }
-    }, [products, filterStates.brand]);
-
-    console.log('filterStates', filterStates)
+    console.log('filterStates', filterStates);
 
     return (
         <div className={styles.layout}>
             <Typography className={styles.title}>Model</Typography>
 
-            <RadioGroup value={filterStates.model} className={styles.radioContainer} >
+            <RadioGroup value={filterStates.model} className={styles.radioContainer}>
 
                 <TextField
                     variant="outlined"
